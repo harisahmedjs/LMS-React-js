ff855a55e5656cf27e5335ff8f1368460594e9e0
HARIS AHMED <136917000+harisahmedjs@users.noreply.github.com>
2024-02-07 23:06:55 +0500

    working on hai

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>hello</div>
+
   )
 }
 
---
+++ src/screens/admission/Admission.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Admission = () => {
+  return (
+    <div>Admission</div>
+  )
+}
