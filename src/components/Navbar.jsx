78ec4a4d2d89bc678989e1382c7860b9b32d2e95
HARIS AHMED <136917000+harisahmedjs@users.noreply.github.com>
2024-02-10 02:17:07 +0500

    working on hai

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,76 +5,69 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
+import Dialog from '@mui/material/Dialog';
+import Button from '@mui/material/Button';
 
-export default function MenuAppBar() {
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState(null);
+export default function MenuAppBar({ data }) {
+  const [openModal, setOpenModal] = React.useState(false);
 
-  const handleChange = (event) => {
-    setAuth(event.target.checked);
+  const handleOpenModal = () => {
+    setOpenModal(true);
   };
 
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
+  const handleCloseModal = () => {
+    setOpenModal(false);
   };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      
       <AppBar position="static" sx={{ backgroundColor: 'black' }}>
-        <Toolbar>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+
+          {/* Menu Icon (on the left) */}
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}
+            sx={{ marginRight: 2 }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Photos
-          </Typography>
-          {auth && (
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-                
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem    onChange={handleChange}>Logout</MenuItem>
 
-              </Menu>
-            </div>
-          )}
+          {/* Title */}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
+            Welcome To LMS
+          </Typography>
+
+          {/* Logout Image (on the right) */}
+          <img
+            src={data.image}
+            alt="Logout"
+            style={{ height: 40, marginLeft: 2, cursor: 'pointer' }}
+            onClick={handleOpenModal}
+          />
+
+          {/* Logout Modal */}
+          <Dialog open={openModal} onClose={handleCloseModal}>
+            <Typography variant="h6" component="div" sx={{ p: 2 }}>
+              Are you sure you want to log out?
+            </Typography>
+            <Button onClick={handleCloseModal} sx={{ mx: 2 }}>
+              Cancel
+            </Button>
+            <Button
+              onClick={() => {
+                // Handle logout logic here
+                handleCloseModal();
+              }}
+              variant="contained"
+              color="primary"
+            >
+              Logout
+            </Button>
+          </Dialog>
+
         </Toolbar>
       </AppBar>
     </Box>
--- src/screens/student/Student.jsx
+++ src/screens/student/Student.jsx
@@ -1,27 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import MenuAppBar from '../../components/Navbar';
 import { getData, auth } from '../../config/firebase/firebasemethods';
 import { onAuthStateChanged } from 'firebase/auth';
 
 const Student = () => {
+  const [arr, setArr] = useState([]);
+
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
-       
         const uid = user.uid;
         console.log(uid);
 
-       
         getData('students', uid).then((res) => {
-          console.log(res);
+          setArr(res);
         });
-      } 
+      }
     });
   }, []);
 
   return (
     <>
-      <MenuAppBar />
+     
+      {console.log(arr)}
+
+    
+      {arr.map((item, index) => (
+        <h1 key={index}>{item.image}</h1>
+      ))}
+
+      
+      <MenuAppBar data={arr} />
     </>
   );
 };
