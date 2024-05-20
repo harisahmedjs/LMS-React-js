dd18b52fa01cee2a335452d3b6ff2e5ba026ae5f
HARIS AHMED <136917000+harisahmedjs@users.noreply.github.com>
2024-05-21 00:45:07 +0500

    almost completed

--- src/screens/admin/allstudents/AllStudents.jsx
+++ src/screens/admin/allstudents/AllStudents.jsx
@@ -1,9 +1,8 @@
-import React , {useState , useEffect} from 'react'
-import { collection, getDocs, where , query} from "firebase/firestore"; 
+import React, { useState, useEffect } from 'react';
+import { collection, getDocs, where, query } from "firebase/firestore"; 
 import { db } from '../../../config/firebase/firebasemethods';
 
 const AllStudents = () => {
-
   const [arr, setArr] = useState([]);
 
   useEffect(() => {
@@ -22,8 +21,25 @@ const AllStudents = () => {
   };
 
   return (
-    <div>AllStudents</div>
-  )
-}
+    <div className="container mt-5">
+      <h1 className="mb-4">All Students</h1>
+      <div className="row">
+        {arr.map((student, index) => (
+          <div key={index} className="col-md-4 mb-4">
+            <div className="card h-100">
+              <div className="card-body">
+                <img src={student.imageUrl} alt="Student" />
+                <div>
+                  <h5 className="card-title">{student.fullName}</h5>
+                  <p className="card-text">Course: {student.course}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default AllStudents
+export default AllStudents;
